345637a15c6e2a153b2061a6761f127c8b37b556
brindosch <edeltraud70@gmx.de>
2017-11-15 23:56:07 +0100

    update

--- .travis/publish.py
+++ .travis/publish.py
@@ -1,13 +1,133 @@
 #!/usr/bin/python3
 import os, shutil, httplib2, simplejson as json
-print("PUBLSH PY CALLED")
-# print some stuff
-MDIR = os.environ["TRAVIS_BUILD_DIR"]
-TC = os.environ["TRAVIS_COMMIT"]
-TCM = os.environ["TRAVIS_COMMIT_MESSAGE"]
 
-print("TBD",MDIR)
-print("TC",TC)
-print("TCM",TCM)
-print("now exits with")
-exit(True)
+# define GH_REP repository; GH_USR the github user; GH_REL_TAG The release tag to use; GH_TOK the access token
+GH_REP = "plugins"
+GH_USR = "brindosch"
+GH_REL_TAG = "1.0.0"
+GH_TOK = os.environ["GH_TOK"]
+# root directory of plugin source folders
+MDIR = os.environ["TRAVIS_BUILD_DIR"]
+# directory where the zips should be saved
+ZIPDIR = os.environ["TRAVIS_BUILD_DIR"]+"/packages"
+
+GH_API = "https://api.github.com"
+GH_REPO = GH_API+"/repos/"+GH_USR+"/"+GH_REP
+GH_TAGS = GH_REPO+"/releases/tags/"+GH_REL_TAG
+GH_ASSETS = GH_REPO+"/releases/assets/"
+GH_UPLOAD = "https://uploads.github.com/repos/"+GH_USR+"/"+GH_REP+"/releases/"
+AUTHHEAD = {'Authorization': 'token '+GH_TOK}
+HEADERS = {'Content-type': 'application/zip'}
+
+TC = os.environ["TRAVIS_COMMIT"]
+
+# not all commit messages needs to be a upload request, filter them
+if "[" not in TC or "]" not in TC:
+	print("Aborting script, nothing to do as there where no [] in commit message")
+	exit()
+
+# add auth header
+HEADERS.update(AUTHHEAD)
+
+ZIPPEDFILES = []
+GITHUB_FILES = []
+REPOINDEX = []
+
+http = httplib2.Http()
+
+# get all module. and service. subdirs in MDIR
+def getSubDirs():
+	dlist = next(os.walk(MDIR))[1]
+	cdlist = []
+	for entry in dlist:
+		if entry.startswith("service.") or entry.startswith("module."):
+			cdlist.append(entry)
+	return cdlist
+
+# load plugin.json of path
+def loadMeta(path):
+	global REPOINDEX
+	try:
+		with open(path+"/plugin.json", encoding='utf-8') as meta_file:
+			REPOINDEX.append(json.loads(meta_file.read()))
+	except Exception as e:
+		print("Failed to open/parse plugin.json of: "+path)
+		print(e)
+		exit(True)
+
+# zip a directory and push info dict to ZIPPEDFILES
+def zipDir(dir):
+	global ZIPPEDFILES
+	shutil.make_archive(ZIPDIR+"/"+dir, 'zip', MDIR+"/"+dir)
+	ZIPPEDFILES.append({"path":ZIPDIR+"/"+dir+".zip","name":dir+".zip"})
+	#print("Zipped dir: "+dir+" to "+ZIPDIR+"/"+dir+".zip")
+
+# check if zip file exists on github
+def existZip(name):
+	for entry in GITHUB_FILES:
+		if entry["name"] == name:
+			return True, entry["id"]
+	return False, ''
+
+# delete a file by id from github
+def delete(id):
+	response, content = http.request(GH_ASSETS+str(id), 'DELETE', headers=AUTHHEAD)
+
+# upload a file to github
+def upload(filePath, fileName):
+	GH_ASSET = GH_UPLOAD+TAG_ID+"/assets?name="+fileName
+	response, content = http.request(GH_ASSET, 'POST', headers=HEADERS, body=open(filePath, "rb"))
+	parsed = json.loads(content)
+	if 'errors' in parsed:
+			errors = parsed["errors"][0]
+			if 'code' in errors:
+				print("There where errors during upload Code: "+errors["code"]+" FileName: "+fileName)
+				exit(True)
+	else:
+		print("Upload was successfull: "+fileName)
+
+# extract plugin id from commit
+pluginid = (TC.split("["))[1].split("]")[0]
+print("Starting processing of plugin id"+pluginid)
+
+# verify commit id is found as folder name
+# zip on success (push to ZIPPEDFILES)
+if pluginid not in getSubDirs():
+	print("The commit plugin id does not exist as folder! -> "+pluginid)
+	exit(True)
+else:
+	zipDir(pluginid)
+
+# get list of github assets and tag id of the specified github tag
+response, content = http.request(GH_TAGS, 'GET', headers=AUTHHEAD)
+parsed = json.loads(content)
+# tag id
+TAG_ID = json.dumps(parsed["id"])
+
+# build list of github assets; push to GITHUB_FILES
+for entry in parsed["assets"]:
+	GITHUB_FILES.append({"name":entry["name"],"id":entry["id"]})
+
+# generate new repository index
+for dir in getSubDirs():
+	loadMeta(MDIR+"/"+dir)
+
+# save to disk
+with open(MDIR+"/repository.json","w+") as f:
+	f.write(json.dumps(REPOINDEX))
+	f.close()
+
+# append it to ZIPPEDFILES, to upload it just if prev uploads where successfull
+ZIPPEDFILES.append({"path":MDIR+"/repository.json","name":"repository.json"})
+
+# upload all entrys in ZIPPEDFILES
+for zfile in ZIPPEDFILES:
+	exists, id = existZip(zfile["name"])
+	if exists:
+		# delete asset
+		delete(id)
+	# upload asset
+	upload(zfile["path"],zfile["name"])
+
+# gracefully ended
+print("Script ended successfully")
---
+++ .travis/test.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+#####
+# Testing script for plugin pull requests to fullfill the requirements
+#####
+import os, simplejson as json
+
+# Errors/Warnings store
+Errors = []
+Warnings = []
+# root directory of plugin source folders
+MDIR = os.environ["TRAVIS_BUILD_DIR"]
+
+# helper methods
+def getSubDirs():
+	dlist = next(os.walk(MDIR))[1]
+	cdlist = []
+	for entry in dlist:
+		if entry.startswith("service.") or entry.startswith("module."):
+			cdlist.append(entry)
+	return cdlist
+
+def addError(test,msg):
+	global Errors
+	Errors.append("TEST: "+test+": "+msg)
+
+def addWarning(test,msg):
+	global Warnings
+	Warnings.append("TEST: "+test+": "+msg)
+
+def printErrors():
+	print("###### TEST FAILED PLEASE FIX ALL ERRORS ######")
+	for err in Errors:
+		print(err)
+	print("###### TEST END ######")
+	exit(True)
+
+# Test commit message for []
+TC = os.environ["TRAVIS_COMMIT"]
+if "[" not in TC or "]" not in TC:
+	addError("COMMIT-HEADER","The pull request header should contain the plugin id in [] example: [service.kodi]")
+	# fast abort, we need the id
+	printErrors()
+
+# Test if plugin id exists as folder plugin id from commit
+prPluginId = (TC.split("["))[1].split("]")[0]
+if prPluginId not in getSubDirs():
+	addError("PLUGIN-ID","Plugin id of the pull request header("+prPluginId+") is not available as folder name. Keep in mind that the id should start with 'service.' or 'module.'")
+	# fast abort, as we need the plugin id to know where to search for meta file
+	printErrors()
+
+
+print("Testing plugin id: "+prPluginId)
+
+# Let's parse the meta data
+try:
+	with open(MDIR+"/"+prPluginId+"/plugin.json", encoding='utf-8') as data_file:
+		parsedMeta = json.loads(data_file.read())
+except Exception as e:
+	addError("plugin.json","Failed to open or parse the plugin.json file. Please make sure the file exist and is valid json. You can test for valid json with www.jsonlint.com")
+	print(e)
+	# fast abort
+	printErrors()
+
+# Compare plugin id of meta file with id of folder
+if parsedMeta["id"] not in getSubDirs():
+	addError("plugin.json-id","The id of your plugin.json does not exist as folder name. Please use the same name for id and folder")
+
+# check if id is lowercase
+if not parsedMeta["id"].islower():
+	addError("plugin.json-id","The id of your plugin.json should be lowercase")
+
+# check if ":" is in id
+if any(i in parsedMeta["id"] for i in '":_#*='):
+	addError("plugin.json-id","The id of your plugin.json shouldn't contain ':' '_' '#' '*' '=' '\"' character")
+
+if "name" not in parsedMeta:
+	addError("plugin.json-name","Please add a name property to your plugin.json. The name is a user friendly display name")
+
+if "description" not in parsedMeta:
+	addError("plugin.json-description","Please add a description property to your plugin.json with a brief description what your plugin does")
+
+if "version" not in parsedMeta:
+	addError("plugin.json-version","Please add a version property to your plugin.json in format of MAJOR.MINOR.PATCH '1.0.0'")
+
+if parsedMeta["id"].startswith("service.") and "dependencies" not in parsedMeta:
+	addWarning("plugin.json-dependencies","It's recommended to add a dependencies property to your plugin.json, at least to define a minimum version of Hyperion.")
+
+if "changelog" not in parsedMeta:
+	addError("plugin.json-changelog",'Please add a changelog property to your plugin.json in format of [{"HeaderFirstEntry":"-Info1 -Info2"},{"HeaderSecondEntry":"-Info1 -Info2"}]')
+
+if "provider" not in parsedMeta:
+	addError("plugin.json-provider",'Please add a provider property to your plugin.jso. This is usually the Auhtor name/nick')
+
+if "support" not in parsedMeta:
+	addError("plugin.json-support",'Please add a support property to your plugin.json. This should be a url that points to a support thread at our forum in format of "https://MyUrl"')
+elif not parsedMeta["support"].startswith("https://"):
+	addError("plugin.json-support",'The support field should contain a URL in format of "https://MyUrl"')
+
+if "source" not in parsedMeta:
+	addError("plugin.json-source",'Please add a source property to your plugin.json. This should be a url that points to the source code of this plugin')
+elif not parsedMeta["source"].startswith("https://"):
+	addError("plugin.json-source",'The source field should contain a URL in format of "https://MyUrl"')
+
+# LICENCE should be defined
+if "licence" not in parsedMeta:
+	addError("plugin.json-licence",'Please add a licence property to your plugin.json. Can be MIT or LGPL for example')
+
+# settingsSchema.json scheck
+if parsedMeta["id"].startswith("service."):
+	if os.path.isfile(MDIR+"/"+parsedMeta["id"]+"/settingsSchema.json"):
+		try:
+			with open(MDIR+"/"+parsedMeta["id"]+"/settingsSchema.json", encoding='utf-8') as schema_file:
+				json.loads(schema_file.read())
+		except Exception as e:
+			addError("settingsSchema.json","Can't open or parse settingsSchema.json. You can check json validation with www.jsonlint.com")
+			print(e)
+	else:
+		addError("settingsSchema.json",'A service. plugin requires a settingsSchema.json file to provide options to configure.')
+
+# now print all errors or exit gracefully
+if Errors:
+	printErrors()
+
+print("###### TEST SUCCESSFULLY PASSED ######")
+if Warnings:
+	print("\n###### WARNINGS ######")
+	for warn in Warnings:
+		print(warn)
