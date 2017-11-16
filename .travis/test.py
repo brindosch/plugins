9e1dfa50642cd0068b4289e5f527f80ac76d5c6d
brindosch <edeltraud70@gmx.de>
2017-11-16 02:56:53 +0100

    [service.another]
    But now you are fixed

--- .travis/test.py
+++ .travis/test.py
@@ -34,10 +34,8 @@ def printErrors():
 	print("###### TEST END ######")
 	exit(True)
 
-output = subprocess.check_output(["git", "log", "-n", "1", "--skip", "1", "--pretty=format:\"%B\""])
-print("OUTPUT:"+output.decode("utf-8"))
 # Test commit message for []
-TC = os.environ["TRAVIS_COMMIT_MESSAGE"]
+TC = subprocess.check_output(["git", "log", "-n", "1", "--skip", "1", "--pretty=format:\"%B\""]) #os.environ["TRAVIS_COMMIT_MESSAGE"] the travis env_var reffers to "Merge hash into hash" on PRs
 if "[" not in TC or "]" not in TC:
 	addError("COMMIT-HEADER","The commit should contain the plugin id in [] example: [service.kodi] The current commit message is: "+TC)
 	# fast abort, we need the id
