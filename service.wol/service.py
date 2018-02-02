7a2ab07048653b3291b686e6bdacf10f4ef8ead0
brindosch <edeltraud70@gmx.de>
2018-02-02 17:31:23 +0100

    [service.sample] V1.0.0 - initial release

--- service.wol/service.py
+++ service.wol/service.py
@@ -1,3 +1,48 @@
+from enum import Enum
+from plugin import *
+import wakeonlan
 import time
 
-time.sleep(7)
+# the settings
+sendCont = False
+macAddresses = ()
+
+def sendPacket(count = 1):
+	"""
+		Send a magic packet to all listed mac addresses, if count is higher than 1 it will be repeated (1s delay in between)
+	"""
+	for i in range(count):
+		for mac in macAddresses:
+			wakeonlan.send_magic_packet(mac)
+		
+		time.sleep(1)
+
+def getUserSettings():
+	""" Get user settings
+		Get the user settings and resend magic packets
+	"""
+	global macAddresses, sendCont
+	settings = getSettings()
+	macAddresses = settings.get('macAddresses', ())
+	sendCont = settings.get('sendContinuous', False)
+	sendPacket(3)
+
+def onSettingsChanged():
+	""" Settings changed
+		Update them!
+	"""
+	getUserSettings()
+
+
+# Get the user settings and send first shot
+getUserSettings()
+
+# Register callbacks from Hyperion to get notifications during runtime about specific events
+registerCallback(ON_SETTINGS_CHANGED, onSettingsChanged)
+
+# use loop to send continuous
+while not abort():
+	time.sleep(1)
+	if sendCont:
+		sendPacket()
+		
